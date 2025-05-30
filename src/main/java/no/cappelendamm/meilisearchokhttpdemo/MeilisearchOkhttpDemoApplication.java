43d5647e128c7a504f9a25b9b103251596cae597
Lars Kristian Haga <lars.kristian.haga@cappelendamm.no>
2025-05-30 12:04:28 +0200

    Initial commit

---
+++ src/main/java/no/cappelendamm/meilisearchokhttpdemo/MeilisearchOkhttpDemoApplication.java
@@ -0,0 +1,13 @@
+package no.cappelendamm.meilisearchokhttpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeilisearchOkhttpDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeilisearchOkhttpDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/cappelendamm/meilisearchokhttpdemo/MeilisearchOkhttpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package no.cappelendamm.meilisearchokhttpdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeilisearchOkhttpDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
